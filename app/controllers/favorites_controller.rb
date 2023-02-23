8d0e68b1a1e88837662c7b2780aa5dff66a71d86
3lviend <henryss_10@yahoo.com>
2023-02-23 17:16:16 +0700

    Skip verify authenticity token when create favorite color

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class FavoritesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[create update_index_favorite]
+
   def index
     @favorites       = Favorite.with_ip_address(request.remote_ip).order(index: :asc)
     @shared          = SharedFavorite.get_shared_favorite(request.remote_ip)&.favorites&.ids || []
